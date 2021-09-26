73e98a59dbdf0a0b3ad09162d46f5a6a8de03588
Ainiall <angela_l.lopez@hotmail.com>
2021-09-26 17:57:30 +0200

    [LAB1] Ejercicio 2

--- aima-gui/src/main/java/aima/gui/demo/search/EightPuzzleDemo.java
+++ aima-gui/src/main/java/aima/gui/demo/search/EightPuzzleDemo.java
@@ -22,24 +22,41 @@ import java.util.Properties;
  */
 
 public class EightPuzzleDemo {
-	private static EightPuzzleBoard boardWithThreeMoveSolution =
-			new EightPuzzleBoard(new int[] { 1, 2, 5, 3, 4, 0, 6, 7, 8 });
+	private static EightPuzzleBoard boardWithThreeMoveSolution = new EightPuzzleBoard(
+			new int[] { 1, 2, 5, 3, 4, 0, 6, 7, 8 });
 
-	private static EightPuzzleBoard random1 =
-			new EightPuzzleBoard(new int[] { 1, 4, 2, 7, 5, 8, 3, 0, 6 });
+	private static EightPuzzleBoard random1 = new EightPuzzleBoard(new int[] { 1, 4, 2, 7, 5, 8, 3, 0, 6 });
 
-//	private static EightPuzzleBoard extreme =
-//			new EightPuzzleBoard(new int[] { 0, 8, 7, 6, 5, 4, 3, 2, 1 });
+	// private static EightPuzzleBoard extreme =
+	// new EightPuzzleBoard(new int[] { 0, 8, 7, 6, 5, 4, 3, 2, 1 });
 
+	// 5 movements
+	private static EightPuzzleBoard boardWith5MovesSolution1 = new EightPuzzleBoard(
+			new int[] { 1, 0, 3, 8, 2, 5, 7, 4, 6 });
+	// 10 movements 
+	private static EightPuzzleBoard boardWith10MovesSolution1 = new EightPuzzleBoard(
+			new int[] { 8, 2, 1, 7, 0, 3, 6, 5, 4 });
+	// 15 movements 
+	private static EightPuzzleBoard boardWith15MovesSolution1 = new EightPuzzleBoard(
+			new int[] { 4, 8, 2, 6, 3, 5, 1, 0, 7 });
+	// 20 movements 
+	private static EightPuzzleBoard boardWith20MovesSolution1 = new EightPuzzleBoard(
+			new int[] { 6, 2, 7, 4, 5, 1, 0, 8, 3 });
+	// 25 movements 
+	private static EightPuzzleBoard boardWith25MovesSolution1 = new EightPuzzleBoard(
+			new int[] { 6, 7, 4, 0, 5, 1, 3, 2, 8 });
+	// 30 movements 
+	private static EightPuzzleBoard boardWith30MovesSolution1 = new EightPuzzleBoard(
+		new int[] {5, 6, 7, 2, 8, 4, 0, 3, 1});
 	public static void main(String[] args) {
 		System.out.println("Initial State:\n" + boardWithThreeMoveSolution);
-		//eightPuzzleDLSDemo();
-		//eightPuzzleIDLSDemo();
-		//eightPuzzleGreedyBestFirstDemo();
-		//eightPuzzleGreedyBestFirstManhattanDemo();
+		// eightPuzzleDLSDemo();
+		// eightPuzzleIDLSDemo();
+		// eightPuzzleGreedyBestFirstDemo();
+		// eightPuzzleGreedyBestFirstManhattanDemo();
 		eightPuzzleAStarDemo();
-		//eightPuzzleAStarManhattanDemo();
-		//eightPuzzleSimulatedAnnealingDemo();
+		eightPuzzleAStarManhattanDemo();
+		// eightPuzzleSimulatedAnnealingDemo();
 	}
 
 	private static void eightPuzzleDLSDemo() {
@@ -72,8 +89,8 @@ public class EightPuzzleDemo {
 		System.out.println("\nEightPuzzleDemo Greedy Best First Search (MisplacedTileHeursitic)");
 		try {
 			Problem<EightPuzzleBoard, Action> problem = new BidirectionalEightPuzzleProblem(boardWithThreeMoveSolution);
-			SearchForActions<EightPuzzleBoard, Action> search = new GreedyBestFirstSearch<>
-					(new GraphSearch<>(), EightPuzzleFunctions::getNumberOfMisplacedTiles);
+			SearchForActions<EightPuzzleBoard, Action> search = new GreedyBestFirstSearch<>(new GraphSearch<>(),
+					EightPuzzleFunctions::getNumberOfMisplacedTiles);
 			SearchAgent<Object, EightPuzzleBoard, Action> agent = new SearchAgent<>(problem, search);
 			printActions(agent.getActions());
 			printInstrumentation(agent.getInstrumentation());
@@ -86,8 +103,8 @@ public class EightPuzzleDemo {
 		System.out.println("\nEightPuzzleDemo Greedy Best First Search (ManhattanHeursitic)");
 		try {
 			Problem<EightPuzzleBoard, Action> problem = new BidirectionalEightPuzzleProblem(boardWithThreeMoveSolution);
-			SearchForActions<EightPuzzleBoard, Action> search = new GreedyBestFirstSearch<>
-					(new GraphSearch<>(), EightPuzzleFunctions::getManhattanDistance);
+			SearchForActions<EightPuzzleBoard, Action> search = new GreedyBestFirstSearch<>(new GraphSearch<>(),
+					EightPuzzleFunctions::getManhattanDistance);
 			SearchAgent<Object, EightPuzzleBoard, Action> agent = new SearchAgent<>(problem, search);
 			printActions(agent.getActions());
 			printInstrumentation(agent.getInstrumentation());
@@ -100,10 +117,10 @@ public class EightPuzzleDemo {
 	private static void eightPuzzleAStarDemo() {
 		System.out.println("\nEightPuzzleDemo AStar Search (MisplacedTileHeursitic)");
 		try {
-			Problem<EightPuzzleBoard, Action> problem = new BidirectionalEightPuzzleProblem(random1);
-			// h1 heuristic getNumberOfMisplacedTiles
-			SearchForActions<EightPuzzleBoard, Action> search = new AStarSearch<>
-					(new GraphSearch<>(), EightPuzzleFunctions::getNumberOfMisplacedTiles);
+			Problem<EightPuzzleBoard, Action> problem = new BidirectionalEightPuzzleProblem(boardWith30MovesSolution1);
+			// h1 heuristic getNumberOfMisplacedTiles  h2 heuristic getManhattanDistance
+			SearchForActions<EightPuzzleBoard, Action> search = new AStarSearch<>(new GraphSearch<>(),
+					EightPuzzleFunctions::getNumberOfMisplacedTiles);
 			SearchAgent<Object, EightPuzzleBoard, Action> agent = new SearchAgent<>(problem, search);
 			printActions(agent.getActions());
 			printInstrumentation(agent.getInstrumentation());
@@ -116,8 +133,8 @@ public class EightPuzzleDemo {
 		System.out.println("\nEightPuzzleDemo Simulated Annealing Search");
 		try {
 			Problem<EightPuzzleBoard, Action> problem = new BidirectionalEightPuzzleProblem(random1);
-			SimulatedAnnealingSearch<EightPuzzleBoard, Action> search = new SimulatedAnnealingSearch<>
-					(EightPuzzleFunctions::getManhattanDistance);
+			SimulatedAnnealingSearch<EightPuzzleBoard, Action> search = new SimulatedAnnealingSearch<>(
+					EightPuzzleFunctions::getManhattanDistance);
 			SearchAgent<Object, EightPuzzleBoard, Action> agent = new SearchAgent<>(problem, search);
 			printActions(agent.getActions());
 			System.out.println("Final State:\n" + search.getLastState());
@@ -130,9 +147,9 @@ public class EightPuzzleDemo {
 	private static void eightPuzzleAStarManhattanDemo() {
 		System.out.println("\nEightPuzzleDemo AStar Search (ManhattanHeursitic)");
 		try {
-			Problem<EightPuzzleBoard, Action> problem = new BidirectionalEightPuzzleProblem(random1);
-			SearchForActions<EightPuzzleBoard, Action> search = new AStarSearch<>
-					(new GraphSearch<>(), EightPuzzleFunctions::getManhattanDistance);
+			Problem<EightPuzzleBoard, Action> problem = new BidirectionalEightPuzzleProblem(boardWith30MovesSolution1);
+			SearchForActions<EightPuzzleBoard, Action> search = new AStarSearch<>(new GraphSearch<>(),
+					EightPuzzleFunctions::getManhattanDistance);
 			SearchAgent<Object, EightPuzzleBoard, Action> agent = new SearchAgent<>(problem, search);
 			printActions(agent.getActions());
 			printInstrumentation(agent.getInstrumentation());
